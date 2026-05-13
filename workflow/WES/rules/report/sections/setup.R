a110abaa04b722b9d2869cc2adcfc2bc9b11015d
Yuliang <zyl915661305@hotmail.com>
2026-05-14 01:23:50 +0800

    Add WES HTML report template and sections
    Introduce a Clindet WES report
    
    Co-Authored-By: Junyi Zhang <zjy112@sjtu.edu.cn>

---
+++ workflow/WES/rules/report/sections/setup.R
@@ -0,0 +1,8 @@
+1. Patient, tumour and sample characteristics
+2. NGS assay and data analysis characteristics
+3. Sample-specific assay performance and quality control
+4. Genomic alterations (somatic and/or germline) andother genomic measures detected in the tumour sample
+5. Variant functional annotation
+6. Matching with genomic biomarkers of cancer diagnosis,prognosis and therapy indication, ranked by clinical actionability
+7. Summary of the main clinically relevant findings
+8. Appendices with detailed information and references
--- workflow/common/scripts/split_maf.R
+++ workflow/common/scripts/split_maf.R
@@ -1,9 +1,9 @@
 library(readr)
 library(dplyr)
 
-input_maf <- snakemake@inputs[['maf']]
-output_snp <- snakemake@inputs[['snp']]
-output_indel <- snakemake@inputs[['indel']]
+input_maf <- snakemake@input[['maf']]
+output_snp <- snakemake@output[['snp']]
+output_indel <- snakemake@output[['indel']]
 
 lines <- readLines(if (grepl("\\.gz$", input_maf)) gzfile(input_maf, "rt") else input_maf)
 
@@ -22,11 +22,15 @@ if (!"Variant_Type" %in% colnames(maf)) {
 maf_snp <- maf %>% filter(Variant_Type == "SNP")
 maf_indel <- maf %>% filter(Variant_Type %in% c("INS", "DEL"))
 
-write_with_comments <- function(df, outfile, comments) {
-    con <- file(outfile, "wt")
-    on.exit(close(con), add = TRUE)
-    if (length(comments) > 0) writeLines(comments, con)
-    write_tsv(df, con)
+write_with_comments <- function(df, outfile, comments = character()) {
+  if (length(comments) > 0) {
+    writeLines(comments, outfile, sep = "\n")
+    write("\n", file = outfile, append = TRUE)
+  } else {
+    file.create(outfile)
+  }
+
+  readr::write_tsv(df, outfile, append = TRUE, col_names = TRUE)
 }
 
 write_with_comments(maf_snp, output_snp, comment_lines)
